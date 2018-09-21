e38cc39e0ee51d7f26e89147c38d901e9c0db1e7
richard <richard@richard-imac2.dbs.dbcorp.nl>
2018-09-21 13:32:59 +0200

    Version 1.5.11 Added Billink payment method.

---
+++ cardgateplus/includes/languages/dutch/modules/payment/cgp_billink.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Copyright (c)2013 Card Gate Plus
+* Author: Richard Schoots
+* For more infomation about Card Gate Plus: http://www.cardgate.com 
+* Released under the GNU General Public License
+* Zen-Cart version Copyright (c) 2011 GetZenned: http://www.getzenned.nl
+*/
+
+  define('MODULE_PAYMENT_CGP_BILLINK_TEXT_TITLE', 'Billink');
+  define('MODULE_PAYMENT_CGP_BILLINK_TEXT_DESCRIPTION', 'Neem contact op met Card Gate Plus (www.cardgate.com) voor informatie');
+  define('MODULE_PAYMENT_CGP_BILLINK_CONFIRMATION_TITLE', '(Via Card Gate Plus)');
+  define('MODULE_PAYMENT_CGP_BILLINK_CONFIRMATION_TEXT', '');
+  define('MODULE_PAYMENT_CGP_BILLINK_TEXT_ERROR', 'Er is een fout opgetreden');
+  define('MODULE_PAYMENT_CGP_BILLINK_TEXT_ERROR_MESSAGE', 'Er is een fout opgetreden bij het verwerken van uw betaling. Probeert u het nogmaals.');
+
+?>
